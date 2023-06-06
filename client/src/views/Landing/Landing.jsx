91b244edc452c39d7122cc2b33745e6252333575
danielpaz23 <danielpaz2233@gmail.com>
2023-06-05 22:39:35 -0300

    new Backend

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -4,8 +4,8 @@ const Landing = ()=>{
     return(
     <div className={style.divreset}>
         <h1>Pi-Countries by Paz Daniel</h1>
-        <p className={style.welcom}>¡Bienvenido/a a nuestra pagina!
-En donde vas a poder encontrar todos los paises con su informacion mas importante, ademas de ver diferentes actividades que se realizan en cada pais. </p>
+        <p className={style.welcom}>¡Bienvenido/a a nuestra página!
+En donde vas a poder encontrar todos los países con su información más importante, además de ver diferentes actividades que se realizan en cada país. </p>
 <p className={style.thanks}>Gracias por tu visita!</p>
         <Link className={style.links} to="/home">¡Comenzar Aventura!</Link>
     </div>
