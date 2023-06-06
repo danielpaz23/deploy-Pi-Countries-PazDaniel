8e890f1d5a37ef6f3891493802b1d098ff1ea955
danielpaz23 <danielpaz2233@gmail.com>
2023-06-06 12:59:46 -0300

    links v12

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link , useHistory} from "react-router-dom"
 import style from "./NavBar.module.css"
 import React from "react";
 export default function Nav() {
+    const history = useHistory();
+
+  const handleHomeClick = () => {
+    if (history.location.pathname === '/home') {
+      window.location.reload();
+    }
+  };
+
     return (
         <nav>
             <div className={style.divContainer}>
-                <Link to="/home" className={style.links}>Home</Link>
+                <Link to="/home" className={style.links} onClick={handleHomeClick}>Home</Link>
                 <Link to="/activities" className={style.links}>Create Activity</Link>
             </div>
         </nav>
